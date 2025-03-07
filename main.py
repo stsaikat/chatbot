80a42080f8a3fa31506d862953cba01d5adc56b2
sunipun talukder <sunipun@sunipuns-MacBook-Air.local>
2025-03-07 10:54:43 +0600

    langchain and ai chatbot added

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import random
+import os
 
-def random_response(message, history):
-    print("Message:", message)
-    print("History:", history)
-    return random.choice(["Yes", "No"])
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import datetime
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import HuggingFaceHub
+
+llm = HuggingFaceHub(repo_id="google/flan-t5-large")
+
+memory = ConversationBufferMemory()
+conversation = ConversationChain(
+    llm=llm, 
+    memory = memory,
+    verbose=False
+)
+
+
+def generate_response(message, history):
+    reponse = conversation.predict(input=message)
+    return reponse
 
 import gradio as gr
 
 gr.ChatInterface(
-    fn=random_response, 
+    fn=generate_response, 
     type="messages"
 ).launch()
